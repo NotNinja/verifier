dab67161b5fa3c03cdd677e52441a5224ab945c6
Alasdair Mercer <amercer@secureworks.com>
2016-10-14 13:13:21 +0100

    completed unit test for ArrayFormatter

--- src/test/java/io/skelp/verifier/message/ArrayFormatterTest.java
+++ src/test/java/io/skelp/verifier/message/ArrayFormatterTest.java
@@ -23,10 +23,53 @@ package io.skelp.verifier.message;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 /**
  * Tests for the {@link ArrayFormatter} class.
  *
  * @author Alasdair Mercer
  */
 public class ArrayFormatterTest {
+
+  @Test
+  public void testFormat() {
+    String expected = "['1', '2', '3']";
+    String actual = new ArrayFormatter<>(new Integer[]{1, 2, 3}).format();
+
+    assertEquals("Formats array", expected, actual);
+  }
+
+  @Test
+  public void testFormatWhenArrayHasSingleItem() {
+    String expected = "['1']";
+    String actual = new ArrayFormatter<>(new Integer[]{1}).format();
+
+    assertEquals("Formats array with single item", expected, actual);
+  }
+
+  @Test
+  public void testFormatWhenArrayIsEmpty() {
+    String expected = "[]";
+    String actual = new ArrayFormatter<>(new Object[0]).format();
+
+    assertEquals("Formats empty array", expected, actual);
+  }
+
+  @Test
+  public void testFormatWhenArrayIsNull() {
+    String expected = "null";
+    String actual = new ArrayFormatter<>(null).format();
+
+    assertEquals("Formats null array", expected, actual);
+  }
+
+  @Test
+  public void testToString() {
+    ArrayFormatter<Integer> formatter = new ArrayFormatter<>(new Integer[]{1, 2, 3});
+    String expected = formatter.format();
+    String actual = formatter.toString();
+
+    assertEquals("Formats array", expected, actual);
+  }
 }
